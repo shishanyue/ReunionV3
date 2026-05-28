77fbd8cfb72792851d99fe897be2de0064a58663
洗玻璃呀 <mcrandom@163.com>
2026-05-28 17:51:56 +0800

    24

---
+++ android/src/main/java/cn/tesseract/soviet/android/mixin/TestMixin.java
@@ -0,0 +1,9 @@
+package cn.tesseract.soviet.android.mixin;
+
+import com.corrodinggames.rts.appFramework.IntroScreen;
+
+import me.shedaniel.staticmixin.spongepowered.asm.mixin.Mixin;
+
+@Mixin(IntroScreen.class)
+public class TestMixin {
+}
